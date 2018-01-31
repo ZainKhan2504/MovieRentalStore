ca3f69835349f824e4be06e5e1b21ce6c579cd40
ZainKhan2504 <zainalikhan2504@gmail.com>
2018-01-31 21:30:50 +0500

    Added CustomerController, CustomerDetail, CustomerIndex Edit MovieController, MovieIndex, Layout and NavBar

---
+++ MovieRentalStore/MovieRentalStore/Controllers/CustomersController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MovieRentalStore.Models;
+
+namespace MovieRentalStore.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customers = GetCustomers();
+            return View(customers);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var customer = GetCustomers().SingleOrDefault(c => c.id == id);
+
+            if (customer == null)
+                return HttpNotFound();
+            return View(customer);
+        }
+
+        private IEnumerable<Customers> GetCustomers()
+        {
+            return new List<Customers>
+            {
+                new Customers {id = 1, Name = "John Smith"},
+                new Customers {id = 2, Name = "Mary Williams"}
+            };
+        }
+    }
+}
--- MovieRentalStore/MovieRentalStore/Controllers/MovieController.cs
+++ MovieRentalStore/MovieRentalStore/Controllers/MovieController.cs
@@ -11,6 +11,20 @@ namespace MovieRentalStore.Controllers
     public class MovieController : Controller
     {
         // GET: Movie
+        public ActionResult Index()
+        {
+            var movies = GetMovies();
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie {Id = 1, Name = "Shrek"},
+                new Movie {Id = 2, Name = "Wall-e"}
+            };
+        }
         public ActionResult Random()
         {
             var movie = new Movie() {Name = "Shrek!"};
