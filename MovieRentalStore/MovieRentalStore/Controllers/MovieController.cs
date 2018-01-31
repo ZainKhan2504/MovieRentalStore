73f5ac17eff0a436e46f40a92b7d047aa10f8ec5
ZainKhan2504 <zainalikhan2504@gmail.com>
2018-01-31 16:28:44 +0500

    Create ViewModel

--- MovieRentalStore/MovieRentalStore/Controllers/MovieController.cs
+++ MovieRentalStore/MovieRentalStore/Controllers/MovieController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using MovieRentalStore.Models;
+using MovieRentalStore.ViewModels;
 
 namespace MovieRentalStore.Controllers
 {
@@ -13,7 +14,17 @@ namespace MovieRentalStore.Controllers
         public ActionResult Random()
         {
             var movie = new Movie() {Name = "Shrek!"};
-            return View(movie);
+            var customers = new List<Customers>
+            {
+                new Customers {Name = "Customer 1" },
+                new Customers {Name = "Customer 2" }
+            };
+            var ViewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+            return View(ViewModel);
         }
     }
 }
---
+++ MovieRentalStore/MovieRentalStore/ViewModels/RandomMovieViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using MovieRentalStore.Models;
+
+namespace MovieRentalStore.ViewModels
+{
+    public class RandomMovieViewModel
+    {
+        public Movie Movie { get; set; }
+        public List<Customers> Customers { get; set; }
+    }
+}
